5a041837a82863dcea03bf317e807e38f0c11174
Anand Joe <yueyi14@hotmail.com>
2022-06-29 16:56:51 +0800

    重命名解决方案

--- Properties/UserData.Designer.cs
+++ Properties/UserData.Designer.cs
@@ -12,7 +12,7 @@ namespace ScanUploader.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.1.0.0")]
     internal sealed partial class UserData : global::System.Configuration.ApplicationSettingsBase {
         
         private static UserData defaultInstance = ((UserData)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new UserData())));
